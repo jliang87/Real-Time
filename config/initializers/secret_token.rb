a8012703cfd48af4d71ae713e32c6303ddee5786
Kyle d'Oliveira <kyle@goclio.com>
2013-04-16 14:55:41 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClioInOutStub::Application.config.secret_token = '9f268cc10987ae69382c42c4492845206f365092e90e971454ce0c890c93ddd7f74bb6dd10ab816a7377dfccf2d9add06a6effe78fa00c6715216df7d3c5b3ff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ClioInOutStub::Application.config.session_store :cookie_store, key: '_clio-in-out-stub_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ClioInOutStub::Application.config.session_store :active_record_store
